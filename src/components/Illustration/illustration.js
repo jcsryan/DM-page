9d7675c16667918cdb4de0eb4d367e76b30eecd7
JCSR <johnconnorsryan@gmail.com>
2020-11-20 15:38:57 -0500

    JT infor added, cody illustrations added

--- src/components/Illustration/illustration.js
+++ src/components/Illustration/illustration.js
@@ -1,9 +1,39 @@
 import React from "react";
-
+import Scene1 from '../assets/cody-clark-scene.jpg';
+import Dav from '../assets/Dav_Color.png'
+import Golem from '../assets/Golem copy.png'
+import Murd from '../assets/Murd.png'
+import Racer from '../assets/Racer.png'
+import Racer2 from '../assets/Racer2.png'
+import Rocky from '../assets/Rocky.png'
+import Thaedris from '../assets/Thae_Color.png'
+import Thortain from '../assets/Thorn_Color.png'
+import Thortain2 from '../assets/Thortain.png'
+import Zinkas from '../assets/Zinkas.png'
 
 function Illustration(){
     return (
-        <p>Success</p>
+        <div className="gallery">
+            <div className="gallery2">
+                <div className="Characters">
+                <img src={Murd} className="char" alt="webpage screenshot"></img> 
+                <img src={Rocky} className="char" alt="webpage screenshot"></img> 
+                <img src={Dav} className="char" alt="webpage screenshot"></img> 
+                <img src={Thaedris} className="char" alt="webpage screenshot"></img> 
+                <img src={Thortain} className="char" alt="webpage screenshot"></img> 
+                <img src={Zinkas} className="char" alt="webpage screenshot"></img> 
+                </div>
+                <div className="Full">
+                <img src={Racer} className="full" alt="webpage screenshot"></img> 
+                <img src={Racer2} className="full" alt="webpage screenshot"></img> 
+
+                </div>
+                <div className="Scenes">
+                <img src={Scene1} className="scene" alt="webpage screenshot"></img> 
+                <img src={Golem} className="scene" alt="webpage screenshot"></img> 
+                </div>
+            </div>
+        </div>
     )
 };
 
--- src/components/Us/us.js
+++ src/components/Us/us.js
@@ -1,9 +1,15 @@
 import React from "react";
+import JT from '../assets/jt.jpg'
+import connor from '../assets/connor.jpg'
 
 function Us() {
     return(
+        <div className='container'>
         <div id="usid" className="us">
             <div class="John-Connor-Ryan">
+                <div className="Portrait">
+                </div>
+                <img src={connor} alt="webpage screenshot" className="headshot"></img> 
                 <h1>John Connor Ryan</h1>
                 <p>I have been playing dungeons and dragons since I was in highschool. For nearly two decades I have walked the worlds
                     of Faerun and Greyhawk, I have played nearly every class for many dungeon masters. Seven years ago I moved and
@@ -15,11 +21,20 @@ function Us() {
                 </p>
             </div>
             <div class="JT">
+                <div className="Portrait">
+                <img src={JT} alt="webpage screenshot" className="headshot"></img>
+                </div> 
                 <h1>JT</h1>
+                <p>I've been running 5th Edition Dungeons and Dragons games for 2+ years now. I thoroughly enjoy running 1st party
+                    modules such as the Lost Mines of Phandelver, Storm Kings Thunder, and Ghost of Saltmarsh.
+                    I would also like to venture out into running campaigns or one shots that are not necessarily tied to
+                    Wizards of the Coasts home campaign setting (The Forgotten Realms.) I like complex combat scenarios that really make the players 
+                    utilize all aspects of their party, survival is not promised.</p>
             </div>
             <div class="cody">
                 <h1>Cody</h1>
             </div>
+        </div>
         </div>
     )
 }
